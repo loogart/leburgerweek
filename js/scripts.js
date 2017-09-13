ad24e627f2d6f4170f5e94daab72b096d04518e3
Chris Soueidan <chris.soueidan@gmail.com>
2017-09-13 10:54:44 -0400

    minor UI updates
    added scroll animation on winners page
    updated media query for left nav winners menu

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
             }
         }
     })
-    
+
     //lightbox for burger photos
     $(".burger-zoomers").magnificPopup({
         delegate: 'a', // child items selector, by clicking on it popup will open
@@ -53,6 +53,29 @@ $(document).ready(function () {
                 return openerElement.is('img') ? openerElement : openerElement.find('img');
             }
         }
+    });
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
     });
 
 });

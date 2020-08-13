0de94eacb3181595872e7671a300057ae6077bf3
Chris Soueidan <chris@loogart.com>
2020-08-12 22:31:56 -0400

    finished html
    - completed burger page
    - completed results page

--- 2020/js/scripts.js
+++ 2020/js/scripts.js
@@ -76,7 +76,7 @@ function initMap() {
         '<div id="content">' +
         '<div id="siteNotice">' +
         '</div>' +
-        '<a href="./burger.html" class="grid-burger__item" target="_blank">' +
+        '<a href="./burger.html" class="grid-burger__item">' +
         '<img src="./img/photo-burger1.jpg" class="img-map" alt="...">' +
         '<span class="resto-name">Restaurant Name</span>' +
         '<span class="burger-name">Poutine Name</span>' +
@@ -111,7 +111,7 @@ function initMap() {
         '<div id="content">' +
         '<div id="siteNotice">' +
         '</div>' +
-        '<a href="./burger.html" class="grid-burger__item" target="_blank">' +
+        '<a href="./burger.html" class="grid-burger__item">' +
         '<img src="./img/photo-burger2.jpg" class="img-map" alt="...">' +
         '<span class="resto-name">Restaurant Name</span>' +
         '<span class="burger-name">Poutine Name</span>' +
@@ -146,7 +146,7 @@ function initMap() {
         '<div id="content">' +
         '<div id="siteNotice">' +
         '</div>' +
-        '<a href="./burger.html" class="grid-burger__item" target="_blank">' +
+        '<a href="./burger.html" class="grid-burger__item">' +
         '<img src="./img/photo-burger3.jpg" class="img-map" alt="...">' +
         '<span class="resto-name">Restaurant Name</span>' +
         '<span class="burger-name">Poutine Name</span>' +
@@ -181,7 +181,7 @@ function initMap() {
         '<div id="content">' +
         '<div id="siteNotice">' +
         '</div>' +
-        '<a href="./burger.html" class="grid-burger__item" target="_blank">' +
+        '<a href="./burger.html" class="grid-burger__item">' +
         '<img src="./img/photo-burger4.jpg" class="img-map" alt="...">' +
         '<span class="resto-name">Restaurant Name</span>' +
         '<span class="burger-name">Poutine Name</span>' +
